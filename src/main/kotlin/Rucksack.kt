a1f6bd386819de0af229f0299a728ae3c2cc7b99
peralty <josiahheinen12@gmail.com>
2023-02-20 16:01:44 +0100

    finish part 2

--- src/main/kotlin/Rucksack.kt
+++ src/main/kotlin/Rucksack.kt
@@ -1,7 +1,8 @@
 data class Rucksack(
     val name: String,
     val firstCompartment: List<Char>,
-    val secondCompartment: List<Char>
+    val secondCompartment: List<Char>,
+    val allItems: List<Char>
 ) {
     fun identifyDuplicate(): Char? {
         return this.firstCompartment.intersect(this.secondCompartment.toSet()).firstOrNull()
--- src/main/kotlin/application.kt
+++ src/main/kotlin/application.kt
@@ -2,8 +2,15 @@ import java.io.File
 
 fun main() {
     val rucksacks = createRucksackListFromInput()
-    val sumOfAllPriorityValues = rucksacks.sumOf { getValueOfDuplicate(it.identifyDuplicate()) }
-    println("Summe aller Prioriäten: $sumOfAllPriorityValues")
+
+    // Part 1
+    val sumOfAllPriorityValues = rucksacks.sumOf { getValueOfItem(it.identifyDuplicate()) }
+    println("Summe aller Prioriäten von allen Duplikaten: $sumOfAllPriorityValues")
+
+    // Part 2
+    val elfGroups = createElfGroupFromRucksackList(rucksacks)
+    val sumOfAllBadgeValues = elfGroups.sumOf { getValueOfItem(identifyBadgeFromElfGroup(it)) }
+    println("Summe aller Prioriäten von Badges: $sumOfAllBadgeValues")
 }
 
 fun createRucksackListFromInput(): List<Rucksack> {
@@ -14,13 +21,39 @@ fun createRucksackListFromInput(): List<Rucksack> {
         val halfLength = line.length / 2
         val firstHalf = line.substring(0, halfLength).toMutableList()
         val secondHalf = line.substring(halfLength).toMutableList()
+        val allItems = firstHalf + secondHalf
         rucksackNameCounter++
-        Rucksack("Rucksack $rucksackNameCounter", firstHalf, secondHalf)
+        Rucksack("Rucksack $rucksackNameCounter", firstHalf, secondHalf, allItems)
     }
 }
 
-fun getValueOfDuplicate(character: Char?): Int {
+fun getValueOfItem(character: Char?): Int {
     val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     val characterToNumericPriorityMap = alphabet.mapIndexed { index, c -> c to index + 1 }.toMap()
     return characterToNumericPriorityMap.getOrDefault(character, 0)
+}
+
+fun createElfGroupFromRucksackList(rucksacks: List<Rucksack>): List<List<Rucksack>> {
+    val allElfGroups = mutableListOf(mutableListOf<Rucksack>())
+    var elfGroup = mutableListOf<Rucksack>()
+
+    rucksacks.forEach { rucksack ->
+        elfGroup.add(rucksack)
+        if (elfGroup.size == 3) {
+            allElfGroups.add(elfGroup)
+            elfGroup = mutableListOf<Rucksack>()
+        }
+    }
+
+    if (elfGroup.isNotEmpty()) {
+        allElfGroups.add(elfGroup)
+    }
+
+    allElfGroups.removeIf { it.isEmpty() }
+    return allElfGroups
+}
+
+fun identifyBadgeFromElfGroup(elfGroup: List<Rucksack>): Char? {
+    val badge = (elfGroup[0].allItems.intersect(elfGroup[1].allItems.toSet())).intersect(elfGroup[2].allItems.toSet())
+    return if (badge.isNotEmpty()) badge.first() else null
 }
